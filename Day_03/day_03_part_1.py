dac8c8b2fabdc52bf93ff33d8cd32cfdd74d0e49
zacharyranes <zachary.ranes@outlook.com>
2022-01-13 12:06:44 -0600

    Day 3 - Part 1 - Done

--- Day_03/day_03_part_1.py
+++ Day_03/day_03_part_1.py
@@ -1,25 +1,46 @@
+"""
+Advent of code 2021 day 3 part 1
+"""
+
+
 def main():
+    """Main Function"""
 
     input_array = []
     with open('input', 'r', encoding="UTF-8") as input_file:
         input_array = input_file.readlines()
 
-    # [x][y] x will be 0 for 0s and 1 for 1s
-    # [x][y] y the col of the input
-    distribution = [[0] * 12, [0] * 12]
+    frequency = [[0] * 12, [0] * 12]
 
     for row in input_array:
-        for i, char in enumerate(row):
-            if char != '\n':
-                if int(char) == 0:
-                    distribution[0][i] += 1
-                if int(char) == 1:
-                    distribution[1][i] += 1
+        for i, c in enumerate(row):
+            if c == "0":
+                frequency[0][i] += 1
+            if c == "1":
+                frequency[1][i] += 1
 
-    print(distribution)
+    gamma_string = ""
+    for i in range(12):
+        if frequency[0][i] > frequency[1][i]:
+            gamma_string += "0"
+        if frequency[0][i] < frequency[1][i]:
+            gamma_string += "1"
 
+    gamma = int(gamma_string, 2)
 
-    #print("Answer: %d" % ())
+    epsilon_string = ""
+    for i in range(12):
+        if frequency[0][i] < frequency[1][i]:
+            epsilon_string += "0"
+        if frequency[0][i] > frequency[1][i]:
+            epsilon_string += "1"
+
+    epsilon = int(epsilon_string, 2)
+
+    print(gamma)
+    print(epsilon)
+
+    print(gamma*epsilon)
 
 
 if __name__ == "__main__":
