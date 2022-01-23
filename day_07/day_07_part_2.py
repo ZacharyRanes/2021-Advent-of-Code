a1c48537abbb1613258427237a85304a44f042de
Zachary Ranes <zachary.ranes@outlook.com>
2022-01-23 16:52:21 -0600

    Day 7 - Both Parts - Done

--- day_07/day_07_part_2.py
+++ day_07/day_07_part_2.py
@@ -1,9 +1,10 @@
 """
-TASK: Advent of code 2021 day 7 part 1
+TASK: Advent of code 2021 day 7 part 2
 
 AUTHOR: Zachary Ranes
 
 NOTE: Bruit force try all end postions for how much fuel they would take to get to
+      This one is very slow with bruit force... but it did find the right answer
 
 TODO: figure out how to do this one without bruit force
 """
@@ -13,10 +14,14 @@ def fuel_used(starting_postions:list, end_postion:int) -> int:
     """
     takes a list of postions and a point all postions should move to
     returns the number of moves for all postions to get to end position
+    now with new fuel cost
     """
     moves_used = 0
     for position in starting_postions:
-        moves_used += abs(position - end_postion)
+        cost = 1
+        for _ in range(1,abs(position - end_postion)+1):
+            moves_used += cost
+            cost += 1
     return moves_used
 
 
@@ -31,7 +36,7 @@ def main():
 
     set_horizontal_positions = set(array_horizontal_positions)
 
-    fuel_needed = fuel_used(array_horizontal_positions, 1)
+    fuel_needed = fuel_used(array_horizontal_positions, 0)
 
     for horizontal_position in set_horizontal_positions:
         temp = fuel_used(array_horizontal_positions, horizontal_position)
