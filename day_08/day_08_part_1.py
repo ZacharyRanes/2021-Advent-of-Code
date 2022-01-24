fd20a53489af046d8228d49e5a9f92ffeafda3b3
Zachary Ranes <zachary.ranes@outlook.com>
2022-01-23 21:02:07 -0600

    Day 8 - Part 1 - Done

--- day_08/day_08_part_1.py
+++ day_08/day_08_part_1.py
@@ -10,7 +10,18 @@ def main():
 
     with open('input', 'r', encoding="UTF-8") as input_file:
         input_array = input_file.readlines()
-        
+
+    modded_input = []
+    for line in input_array:
+        modded_input.append(line.split())
+
+    count = 0
+    for line in modded_input:
+        for i in range(-4,0):
+            if len(line[i]) in (2,4,3,7):
+                count += 1
+
+    print(count)
 
 
 if __name__ == "__main__":
