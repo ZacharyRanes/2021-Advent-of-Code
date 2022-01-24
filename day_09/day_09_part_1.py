64ba6fb67ca4c6fb94aa33dc9ef689d713133e0b
zacharyranes <zachary.ranes@outlook.com>
2022-01-24 09:11:42 -0600

    Day 9 - Files added

---
+++ day_09/day_09_part_1.py
@@ -0,0 +1,16 @@
+"""
+TASK: Advent of code 2021 day 9 part 1
+
+AUTHOR: Zachary Ranes
+"""
+
+
+def main():
+    """Main Function"""
+
+    with open('input', 'r', encoding="UTF-8") as input_file:
+        input_array = input_file.readlines()
+
+
+if __name__ == "__main__":
+    main()
