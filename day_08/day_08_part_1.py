a5c3d8f3831d8a7d33f766fa6d22068ff9f8fd89
Zachary Ranes <zachary.ranes@outlook.com>
2022-01-23 20:42:44 -0600

    Day 8 - Part 1 - Fresh files

---
+++ day_08/day_08_part_1.py
@@ -0,0 +1,17 @@
+"""
+TASK: Advent of code 2021 day 8 part 1
+
+AUTHOR: Zachary Ranes
+"""
+
+
+def main():
+    """Main Function"""
+
+    with open('input', 'r', encoding="UTF-8") as input_file:
+        input_array = input_file.readlines()
+        
+
+
+if __name__ == "__main__":
+    main()
